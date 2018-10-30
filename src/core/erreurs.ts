fdb731422d86ffbbf9c9d47a5922df484f4af5d6
aTewan <tewan33@gmail.com>
2018-10-30 21:24:27 +0100

    fichier erreur + code couleur

---
+++ src/core/erreurs.ts
@@ -0,0 +1,8 @@
+import chalk from 'chalk'
+
+const err = `Erreur : `
+const war = `Attention : `;
+
+export const VALEUR_PAS_NUMERIQUE: String = chalk.bold.red(`${err} La valeur renseignée pour le port n'est pas une valeur numérique.`);
+
+export const PAS_DE_PORT: String = chalk.bold.yellow(`${war} Vous n'avez pas renseigné de valeur pour le port, la valeur par défaut est`)
--- src/core/server.ts
+++ src/core/server.ts
@@ -4,6 +4,8 @@ import * as path from 'path'
 
 import { isNumber, isString } from 'util';
 
+import { VALEUR_PAS_NUMERIQUE, PAS_DE_PORT } from './erreurs'
+
 
 export class Server {
 
@@ -14,9 +16,14 @@ export class Server {
     public async run() {
         this.app = express();
         this.port = await this.recupererPort();
-        this.app.listen(this.port, () => {
-            console.log(`http://localhost:${this.port}`);
-        })
+        if(this.port !== undefined) {
+            this.app.listen(this.port, () => {
+                console.log(`http://localhost:${this.port}`);
+            })
+        }
+        else {
+            console.log(VALEUR_PAS_NUMERIQUE)
+        }
     }
 
     private recupererPort(): Number {
@@ -27,53 +34,18 @@ export class Server {
         
         if(json_port !== null) {
             if(isNumber(json_port)) {
-                console.log(`Port NOMBRE : ${json_port}`)
                 PORT = json_port;
             }
             else if(isString(json_port)) {
-                if(isNaN(Number(json_port))) {
-                    console.log(`Erreur : La valeur renseignée pour le port n'est pas une valeur numérique.`)
-                }
-                else {
-                    console.log(`Port STRING : ${json_port}`)
+                if(!isNaN(Number(json_port))) {
                     PORT = Number(json_port);
                 }
             }
         }
         else {
             PORT= 3000;
-            console.log(`Erreur : Vous n'avez pas renseigné de valeur pour le port, le port par défaut, ${PORT}, est donc utilisé`)
+            console.log(`${PAS_DE_PORT} ${PORT}`)
         }
-        /*
-        fs.readFile(p,'utf8', (err,data) => {
-            if(err) {
-                console.log(`Erreur ${err}`)
-            }
-            else {
-                let json: any = JSON.parse(data);
-                let json_port: any = json.port;
-                if(json_port !== null) {
-                    if(isNumber(json_port)) {
-                        console.log(`Port NOMBRE : ${json_port}`)
-                        PORT = json_port;
-                    }
-                    else if(isString(json_port)) {
-                        if(isNaN(Number(json_port))) {
-                            console.log(`Erreur : La valeur renseignée pour le port n'est pas une valeur numérique.`)
-                        }
-                        else {
-                            console.log(`Port STRING : ${json_port}`)
-                            PORT = Number(json_port);
-                        }
-                    }
-                }
-                else {
-                    PORT= 3000;
-                    console.log(`Erreur : Vous n'avez pas renseigné de valeur pour le port, le port par défaut, ${PORT}, est donc utilisé`)
-                }
-            }
-        });
-        */
         return PORT;
     }
 }
